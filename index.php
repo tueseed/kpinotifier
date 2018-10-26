65713d710148cf58b7e366a98eae3f3e6607af8d
tueseed <tue_seed@hotmail.com>
2018-10-26 14:19:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,63 @@
-<?php
-  echo "KPI Notifier";
-?>
+<html>
+	<head>
+		<title>KPI-NOTIFIER</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0," charset="utf-8">
+		<title>BOT TRAIN PEA S.1</title>
+		<link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+		<style type="text/css">
+			.row-center
+			{
+				text-align:center;
+			}
+			container-fluid 
+			{
+				font-family: 'Kanit', sans-serif;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+			$server = "us-cdbr-iron-east-01.cleardb.net";
+			$username = "b946a03b24557c";
+			$password = "0000fb9a";
+			$db = "heroku_91b59e978ea198b";
+			$conn = new mysqli($server, $username, $password, $db);
+			mysqli_query($conn, "SET NAMES utf8");
+		?>
+		<div class="container-fluid" style="background-color:#b461fb;">
+			<div class="row row-center">
+				<div class="col-lg-4 offset-lg-4" style="background-color:#b461fb;">
+					<h4>KPI-NOTIFIER</h4>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="col-lg-4">
+				<div class="panel-group">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" href="#collapse1">FINANCE</a>
+							</h4>
+						</div>
+						<div id="collapse1" class="panel-collapse collapse">
+							<ul class="list-group">
+								<li class="list-group-item">One</li>
+								<li class="list-group-item">Two</li>
+								<li class="list-group-item">Three</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-8">
+			</div>
+		</div>
+	</body>
+</hmtl>
