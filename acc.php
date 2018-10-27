ddc64a0ca5eff3a76334d7cd3043fb7af8c3b541
tueseed <tue_seed@hotmail.com>
2018-10-27 07:49:12 +0700

    Add files via upload

--- acc.php
+++ acc.php
@@ -22,6 +22,40 @@
 			{
 				font-family: 'Kanit', sans-serif;
 			}
+			.sidenav {
+						height: 100%;
+						width: 0;
+						position: fixed;
+						z-index: 1;
+						top: 0;
+						left: 0;
+						background-color: #111;
+						overflow-x: hidden;
+						transition: 0.5s;
+						padding-top: 60px;
+					}
+			.sidenav a {
+				padding: 8px 8px 8px 32px;
+				text-decoration: none;
+				font-size: 25px;
+				color: #818181;
+				display: block;
+				transition: 0.3s;
+			}
+			.sidenav a:hover {
+				color: #f1f1f1;
+			}
+			.sidenav .closebtn {
+				position: absolute;
+				top: 0;
+				right: 25px;
+				font-size: 36px;
+				margin-left: 50px;
+			}
+			@media screen and (max-height: 450px) {
+			  .sidenav {padding-top: 15px;}
+			  .sidenav a {font-size: 18px;}
+			}
 		</style>
 	</head>
 	<body>
@@ -42,6 +76,9 @@
 			</div>
 		<div class="container-fluid" style="background-color:#b461fb;">
 			<div class="row row-center">
+				<div class="col-lg-1" style="background-color:#b461fb;">
+					<button class="btn"><i class="fa fa-bars" onclick="openNav()"></i> Menu</button>
+				</div>
 				<div class="col-lg-4 offset-lg-4" style="background-color:#b461fb;">
 					<h4>MY MONEY</h4>
 				</div>
