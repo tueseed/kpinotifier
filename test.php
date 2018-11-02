e98e200279c612f17daeaf1d42dc49b5f550d1b9
tueseed <tue_seed@hotmail.com>
2018-11-02 16:28:16 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 	$data = "สายยึด";
 	$dat1 = base64_encode($data);
+	echo "<a href='https://nutt-i.com/psqv2/result.php?keyword='".$dat1."></a>";
 	echo $dat1."<br>";
 	echo base64_decode($dat1);
 ?>
