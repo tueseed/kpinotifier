88846fdba05ee9ad302909be0f00037f9c87a484
tueseed <tue_seed@hotmail.com>
2018-11-03 19:31:34 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -1,7 +1,16 @@
 <?php
-	$data = "สายยึด";
-	$dat1 = base64_encode($data);
-	echo "<a href='https://nutt-i.com/psqv2/result.php?keyword=".$dat1."'>131561215</a>";
-	echo $dat1."<br>";
-	echo base64_decode($dat1);
+			$server = "us-cdbr-iron-east-01.cleardb.net";
+			$username = "bb638a0b9e5724";
+			$password = "3556cc19";
+			$db = "heroku_5663ecc9ac15f3e";
+			$conn = new mysqli($server, $username, $password, $db);
+			mysqli_query($conn, "SET NAMES UTF8");
+			$data = "โพธาราม";
+			$sql = "SELECT * FROM tbl_improve WHERE detail LIKE '%".$data."%'";
+			$query = mysqli_query($conn,$sql);
+			$a=1;
+			while($obj = mysqli_fetch_array($query))
+			{
+				echo $a.".".$obj["office"]."<br>";
+			}
 ?>
