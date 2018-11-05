1409822c8c212171e517c7b228413f9618355183
tueseed <tue_seed@hotmail.com>
2018-11-05 10:11:15 +0700

    Add files via upload

--- send.php
+++ send.php
@@ -59,7 +59,7 @@
                     contentType: false,
                     success: function(response) {
 						var obj = JSON.parse(response);
-                        alert(obj[0].topic);
+                        alert(obj[0]);
                     }
                     
                 });
