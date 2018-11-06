e5fd2fb2495dd0d6d9c797283b1d6069212cbdd7
tueseed <tue_seed@hotmail.com>
2018-11-06 16:11:46 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
 										var opt1 = document.getElementById("topic_submain");
 										opt1.options.length = 0;
 										var obj = jQuery.parseJSON(response);
-										var i = 0;
+										var i = 1;
 										//alert(obj[1].topic);
 										opt1.options[0] = new Option('เลือกหัวข้อย่อย 1','เลือกหัวข้อย่อย 1');
 										while(obj[i].topic)
@@ -130,7 +130,7 @@
 											opt1.options[i] = new Option(obj[i].topic,obj[i].topic);
 											i++;
 										}
-										//opt1.options[3] = new Option('เพิ่มหัวข้อใหม่','เพิ่มหัวข้อใหม่');
+										opt1.options[i] = new Option('เพิ่มหัวข้อใหม่','เพิ่มหัวข้อใหม่');
 									}
 									
 								});
