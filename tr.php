3d9edbd3693c08cc60f101eaff95b43904a2bb94
tueseed <tue_seed@hotmail.com>
2019-01-24 14:52:49 +0700

    Add files via upload

--- tr.php
+++ tr.php
@@ -5,4 +5,10 @@
 	$db = "heroku_750bf9de350a63c";
 	$conn = new mysqli($server, $username, $password, $db);
 	mysqli_query($conn, "SET NAMES utf8");
+	$sql_text = "SELECT * FROM tbl_tr-load";
+	$query = mysqli_query($conn,$sql_text);
+	while($obj = mysqli_fetch_array($query))
+	{
+		echo $obj["pea"];
+	}
 ?>
